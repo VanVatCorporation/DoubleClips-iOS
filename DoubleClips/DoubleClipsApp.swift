22551938bb9c54b05ec280defcd086a637660acf
nguyenviet <viet2007ht@gmail.com>
2026-01-05 07:09:45 +0700

    Initial Commit

---
+++ DoubleClips/DoubleClipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleClipsApp.swift
+//  DoubleClips
+//
+//  Created by Nguyen Viet on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct DoubleClipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
